17276249d57e96f9498a2b76b1a665f57c94a78c
ange1castillo <angelac1357@gmail.com>
2024-06-18 17:11:51 -0700

    Project Setup

---
+++ Synthesizer/test/source/AudioProcessorTest.cpp
@@ -0,0 +1,9 @@
+#include <Gello Equalizer/PluginProcessor.h>
+#include <gtest/gtest.h>
+
+namespace audio_plugin_test {
+  TEST(AudioProcessor, Foo) {
+    audio_plugin::AudioPluginAudioProcessor processor{};
+    ASSERT_FALSE(true);
+  }
+} // namespace audio_plugin_test
