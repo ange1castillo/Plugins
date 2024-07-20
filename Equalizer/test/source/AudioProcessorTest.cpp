a8b80f84a945faf28df08b9f76ca8e296af807db
ange1castillo <angelac1357@gmail.com>
2024-07-20 02:16:03 -0700

    Setup Equalizer

---
+++ Equalizer/test/source/AudioProcessorTest.cpp
@@ -0,0 +1,9 @@
+#include <PluginProcessor.h>
+#include <gtest/gtest.h>
+
+namespace audio_plugin_test {
+  TEST(AudioProcessor, Foo) {
+    AudioPluginAudioProcessor processor{};
+    ASSERT_FALSE(true);
+  }
+} // namespace audio_plugin_test
