501e7aa0e59dcf75150a7f4188de2f7a49bd6c57
ange1castillo <angelac1357@gmail.com>
2024-07-29 00:39:41 -0700

    Added Slider to GUI

--- Equalizer/plugin/include/PluginEditor.h
+++ Equalizer/plugin/include/PluginEditor.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <JuceHeader.h>
 #include "PluginProcessor.h"
+
+struct CustomRotarySlider : juce::Slider
+{
+    CustomRotarySlider() : juce::Slider (juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                         juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+
+    }
+};
 
 //==============================================================================
 class EqualizerAudioProcessorEditor final : public juce::AudioProcessorEditor
@@ -17,6 +27,16 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     EqualizerAudioProcessor& processorRef;
+
+    CustomRotarySlider peakFreqSlider {};
+    CustomRotarySlider peakGainSlider {};
+    CustomRotarySlider peakQualitySlider {};
+    CustomRotarySlider lowCutFreqSlider {};
+    CustomRotarySlider highCutFreqSlider {};
+    CustomRotarySlider lowCutSlopeSlider {};
+    CustomRotarySlider highCutSlopeSlider {};
+
+    std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EqualizerAudioProcessorEditor)
 };
